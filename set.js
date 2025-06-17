07f06a6f9a7259baf2ea3242cc4cf924453f1c11
Quckx <wambuguw75@gmail.com>
2025-06-17 17:20:17 +0300

    Update set.js

--- set.js
+++ set.js
@@ -3,23 +3,23 @@
 There's no need to configure this if you're deploying via Heroku — just set them in the environment variables.*/
 
 const sessionName = 'session';
-const session = process.env.SESSION || '';
+const session = process.env.SESSION || 'BLACK MD;;;cqMVkR4a#lDctpn0PvX3UwU7O7Vxr6BzGqnuOuefCo_UBnhgYIUU';
 const autobio = process.env.AUTOBIO || 'FALSE';
 const autolike = process.env.AUTOLIKE_STATUS || 'TRUE';
 const autoviewstatus = process.env.AUTOVIEW_STATUS || 'TRUE';
 const welcomegoodbye = process.env.WELCOMEGOODBYE || 'FALSE';
 const prefix = process.env.PREFIX || '';
-const appname = process.env.APP_NAME || '';
+const appname = process.env.APP_NAME || 'Quckx;
 const herokuapi = process.env.HEROKU_API;
 const gptdm = process.env.GPT_INBOX || 'FALSE';
 const mode = process.env.MODE || 'PRIVATE';
-const anticall = process.env.AUTOREJECT_CALL || 'TRUE';
-const botname = process.env.BOTNAME || '𝐁𝐋𝐀𝐂𝐊𝐌𝐀𝐂𝐇𝐀𝐍𝐓 𝐁𝐎𝐓';
-const antibot = process.env.ANTIBOT || 'FALSE';
+const anticall = process.env.AUTOREJECT_CALL || 'FALSE';
+const botname = process.env.BOTNAME || 'QUCKX 𝐁𝐎𝐓';
+const antibot = process.env.ANTIBOT || 'TRUE';
 const author = process.env.STICKER_AUTHOR ||'𝗕𝗢𝗧';
-const packname = process.env.STICKER_PACKNAME || '𝐁𝐋𝐀𝐂𝐊𝐌𝐀𝐂𝐇𝐀𝐍𝐓 𝐁𝐎𝐓';
+const packname = process.env.STICKER_PACKNAME || 'QUCKX 𝐁𝐎𝐓';
 const antitag = process.env.ANTITAG || 'TRUE';
-const dev = process.env.DEV || '254114283550';
+const dev = process.env.DEV || '254741551446;
 const menulink = process.env.MENU_LINK || 'https://files.catbox.moe/jxxwms.jpeg';
 const menu = process.env.MENU_TYPE || 'IMAGE';
 const DevRaven = dev.split(",");
